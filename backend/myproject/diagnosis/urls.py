690e2422ed5a24fc52b16640dcb7bfbfeb06365e
Hamza Mohammed Mostafa <167645878+HMMS1@users.noreply.github.com>
2026-04-28 17:23:13 +0300

    urls.py

--- backend/myproject/diagnosis/urls.py
+++ backend/myproject/diagnosis/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import upload_plant_image, my_plant_images
-from .views import analyze_soil, my_soil_analyses
+from .views import (
+    upload_plant_image,
+    my_plant_images,
+    analyze_soil,
+    my_soil_analyses,
+)
 
 urlpatterns = [
-    path("api/diagnosis/upload/", upload_plant_image),
-    path("api/diagnosis/my-images/", my_plant_images),
-     path("api/soil/analyze/", analyze_soil),
-      path("api/soil/analyze/", my_soil_analyses),
-     
+    path("api/diagnosis/upload/", upload_plant_image, name="upload_plant_image"),
+    path("api/diagnosis/my-images/", my_plant_images, name="my_plant_images"),
 
+    path("api/soil/analyze/", analyze_soil, name="analyze_soil"),
+    path("api/soil/my-analyses/", my_soil_analyses, name="my_soil_analyses"),
 ]
