87fda9338c6f837ab0ec8951a5ab64203ebe9caa
Hamza Mohammed Mostafa <167645878+HMMS1@users.noreply.github.com>
2026-04-28 18:49:08 +0300

    auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8000";
+const baseURL = process.env.REACT_APP_API_URL;
+
 
 export const loginRequest = async (payload) => {
-  const res = await axios.post(`${baseURL}/api/auth/login/`, payload, {
+  const res = await axios.post(`${baseURL}/api/login/`, payload, {
     headers: { "Content-Type": "application/json" },
   });
   return res.data;
 };
 
+
 export const registerRequest = async (payload) => {
-  const res = await axios.post(`${baseURL}/api/auth/register/`, payload, {
+  const res = await axios.post(`${baseURL}/api/register/`, payload, {
     headers: { "Content-Type": "application/json" },
   });
   return res.data;
