439664dfce6e0f861f39fb6f4206afe93397d9b9
Hamza Mohammed Mostafa <167645878+HMMS1@users.noreply.github.com>
2026-06-12 16:16:21 +0300

    Update urls.py

--- backend/myproject/myproject/urls.py
+++ backend/myproject/myproject/urls.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
@@ -5,17 +6,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 def home_view(request):
-    return HttpResponse("Napaty Backend is running ✅")
+    return HttpResponse("Napaty Backend is running ")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home_view, name="home"),
-
     path("", include("accounts.urls")),
     path("", include("diagnosis.urls")),
     path("", include("communication.urls")),
     path("api/", include("crop_recommendation.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
